a9f8e5a471ee23a7bdf57d0d43d9a6b6e47f1353
harshit-exe <nikamharshit55@gmail.com>
2024-12-24 00:59:10 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,13 +21,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <>
-      <Analytics/>
     <SocketProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
         <Toaster/>
+        <Analytics/>
         {children}
       </body>
     </html>
